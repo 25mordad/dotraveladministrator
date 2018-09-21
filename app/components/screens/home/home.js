8bb859ca1868aa31042b9b173c0d4e868757a095
Matthew <mmartinezmateo93@gmail.com>
2018-09-21 15:05:42 +0200

    first commit

--- app/components/screens/home/home.js
+++ app/components/screens/home/home.js
@@ -2,11 +2,31 @@ import React, {Component} from 'react';
 import {AsyncStorage, Platform, View,Button,ActivityIndicator} from 'react-native';
 import t from 'tcomb-form-native'; // 0.6.9
 import styles from './style/styles'; // 0.6.9
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+import {GoogleSignin, GoogleSigninButton, statusCodes} from 'react-native-google-signin';
 
 
-export  class Home extends Component {
+export class Home extends Component {
 
+// Somewhere in your code
+
+
+signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    this.setState({ userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      // operation (f.e. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
 
  static navigationOptions = {
     title: 'Log In',
@@ -22,7 +42,7 @@ export  class Home extends Component {
 
      return (
      <View>
-       <GoogleSigninButton
+        <GoogleSigninButton
           style={{ width: 48, height: 48 }}
           size={GoogleSigninButton.Size.Icon}
           color={GoogleSigninButton.Color.Dark}
