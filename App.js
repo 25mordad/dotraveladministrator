eee9ca4161cbc73f3d50b1d328f72b58c548f147
matthew32 <mmartinezmateo93@gmail.com>
2018-10-26 14:14:43 +0200

    upload original files again

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    ); 
   }
 }
 
